4b84974e4c7d438399c86866e55ec1eaa105643f
Lukonzo <lukonzo.david@gmail.com>
2023-09-22 14:43:17 -0700

    Betty doc

--- _printf.c
+++ _printf.c
@@ -10,35 +10,35 @@
 
 int _printf(const char *format, ...)
 {
-        int char_cnt = 0;
-        va_list args;
+	int char_cnt = 0;
+	va_list args;
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (*format)
-        {
-                if (*format == '%' && format[1])
-                {
-                        format++;
-                        if (*format == 'c' || *format == '%')
-                                putchar(va_arg(args, int)), char_cnt++;
-                        else if (*format == 's')
-                        {
-                                char *s = va_arg(args, char *);
+	while (*format)
+	{
+		if (*format == '%' && format[1])
+		{
+			format++;
+			if (*format == 'c' || *format == '%')
+				putchar(va_arg(args, int)), char_cnt++;
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
 
-                                while (s && *s)
-                                        putchar(*s), s++, char_cnt++;
-                        }
+				while (s && *s)
+					putchar(*s), s++, char_cnt++;
+			}
 			else
-                        {
-                                putchar('%'), char_cnt++;
-                                putchar(*format), char_cnt++;
-                        }
-                }
- 		else
-                        putchar(*format), char_cnt++;
-                format++;
-        }
-        va_end(args);
-        return (char_cnt);
+			{
+				putchar('%'), char_cnt++;
+				putchar(*format), char_cnt++;
+			}
+		}
+		else
+			putchar(*format), char_cnt++;
+		format++;
+	}
+	va_end(args);
+	return (char_cnt);
 }
